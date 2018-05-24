c5b40534d27120e6d861253b43b10c98c6a7beb0
umangnarwal <iit2015070@iiita.ac.in>
2018-05-24 05:32:23 +0530

    ScreenViewer

---
+++ RemoteServer/src/remoteserver/Commands.java
@@ -0,0 +1,24 @@
+
+
+package remoteserver;
+
+/**
+ * Used to represent commands which can be sent by the server
+ */
+public enum Commands {
+    PRESS_MOUSE(-1),
+    RELEASE_MOUSE(-2),
+    PRESS_KEY(-3),
+    RELEASE_KEY(-4),
+    MOVE_MOUSE(-5);
+
+    private int abbrev;
+
+    Commands(int abbrev){
+        this.abbrev = abbrev;
+    }
+
+    public int getAbbrev(){
+        return abbrev;
+    }
+}
